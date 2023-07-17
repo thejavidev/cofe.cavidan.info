9a40d514a012617702ba505f0d3aaec3ce70d8fa
thejavidev <javidation.l@gmail.com>
2023-07-17 18:54:30 +0400

    14

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,23 +9,23 @@ const Home = () => {
   return (
     <>
       <div className='bg-[#331C00]'>
-        <section className="section-1 h-[850px]  flex items-center overflow-hidden">
+        <section className="section-1 h-[850px] lg:h-[400px] md:h-full md:pt-[40px]  flex items-center overflow-hidden">
           <Container fluid>
             <Row className='items-center'>
-              <Col lg={6} className='pl-[150px]'>
-                <div className="flex items-start justify-start h-full  flex-col">
-                  <h1 className='text-[#fff] text-[72px] font-[600]'>Make your day <br />with coffee
+              <Col lg={6} md={6} className='pl-[150px] xl:pl-[40px]'>
+                <div className="flex items-start md:items-center md:justify-center justify-start h-full  flex-col">
+                  <h1 className='text-[#fff] text-[72px] font-[600] xl:text-[42px] md:text-center'>Make your day <br />with coffee
                   </h1>
-                  <p className='text-[32px] text-[#fff] mt-[20px]'>Don’t wait in the shop!</p>
-                  <div className="search mt-[50px] w-[500px] ">
-                    <Link to='/menu' className='bg-[#936532] text-[#fff] pt-[15px] pb-[15px] pl-[20px] pr-[20px] text-[20px] rounded-[10px] capitalize'>
+                  <p className='text-[32px] xl:text-[25px] text-[#fff] mt-[20px]'>Don’t wait in the shop!</p>
+                  <div className="search mt-[50px]">
+                    <Link to='/menu' className='bg-[#936532]  text-[#fff] pt-[15px] pb-[15px] pl-[20px] pr-[20px] text-[20px] rounded-[10px] capitalize'>
                       Cofeshoplar
                     </Link>
                   </div>
                 </div>
               </Col>
-              <Col lg={6} className='bg-[--bg] bg-right'>
-                <div className="flex items-center justify-start relative bg-right-alt">
+              <Col lg={6} md={6} className='bg-[--bg] bg-right md:bg-transparent'>
+                <div className="flex items-center justify-start md:justify-center relative bg-right-alt">
                   <div className="relative flex items-center justify-center">
                     <img className='image' src={coffee2} alt="" />
                     <img className='beans' src={beans2} alt="" />
@@ -38,11 +38,11 @@ const Home = () => {
         <section id="2" className='mt-[100px] pb-[170px]'>
           <Container className='bg-[#936532] rounded-[48px] pt-[30px] pb-[30px]'>
             <div className="text flex flex-col items-center justify-center mb-[50px]">
-              <h1 className='text-[42px] font-[600] text-[#fff]'>How Taky works?</h1>
-              <h1 className='text-[42px] font-[600] text-[#fff]'>So simple!</h1>
+              <h1 className='text-[42px] font-[600] text-[#fff] xl:text-[30px]'>How Taky works?</h1>
+              <h1 className='text-[42px] font-[600] text-[#fff] xl:text-[30px]'>So simple!</h1>
             </div>
             <Row className='mt-[30px]'>
-              <Col lg={4}>
+              <Col lg={4} md={12} className='lg:mb-[30px]'>
                 <div className="flex items-center justify-center h-full flex-col pl-[50px] pr-[50px]">
                   <div className="top pb-[20px]">
                     <img src={map} alt="" />
@@ -56,7 +56,7 @@ const Home = () => {
                   </div>
                 </div>
               </Col>
-              <Col lg={4}>
+              <Col lg={4} md={12} className='lg:mb-[30px]'>
                 <div className="flex items-center justify-center h-full flex-col pl-[50px] pr-[50px]">
                   <div className="top pb-[20px]">
                     <img src={coffe_w} alt="" />
@@ -70,7 +70,7 @@ const Home = () => {
                   </div>
                 </div>
               </Col>
-              <Col lg={4}>
+              <Col lg={4} md={12} className='lg:mb-[30px]'>
                 <div className="flex items-center justify-center h-full flex-col pl-[50px] pr-[50px]">
                   <div className="top pb-[20px]">
                     <img src={hand} alt="" />
